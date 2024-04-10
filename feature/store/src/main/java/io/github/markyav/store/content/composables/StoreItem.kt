16f0f711610477845f946fe58983726b3765734d
Mark Yavorskyi <yavorskiymark@gmail.com>
2024-04-10 09:15:58 +0300

    Fixed compilation error

--- feature/store/src/main/java/io/github/markyav/store/content/composables/StoreItem.kt
+++ feature/store/src/main/java/io/github/markyav/store/content/composables/StoreItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,7 +23,6 @@ import androidx.compose.ui.unit.dp
 import io.github.markyav.store.item.SavedSketchItem
 import io.github.markyav.ui.util.AndroidPreviewDevices
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StoreItem(
     item: SavedSketchItem,
